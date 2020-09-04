24b45f93d4f06bbff98f24c9fced5d604ffa0cd8
Manodasan Wignarajah <mawign@microsoft.com>
2020-09-04 06:19:09 -0700

    C++/WinRT app with winmd reference.

---
+++ CppTestApp/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace MyRandom;
+
+int main()
+{
+    init_apartment();
+
+    NumberGenerator numGenerator;
+    printf("Unbounded Number     : %d\n", numGenerator.GetInteger());
+    printf("Bounded Number (0,10): %d\n", numGenerator.GetInteger(0, 100));
+
+    MessageOfTheDayGenerator messageOfTheDayGenerator;
+    printf("Message: %ls\n", messageOfTheDayGenerator.MessageOfTheDay().c_str());
+}
